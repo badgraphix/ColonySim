eae93d156a6eb79b1b37156f0e8058fe2f6eb417
Aidan Hilt <aidanhilt2@gmail.com>
2019-02-13 23:14:41 -0500

    Initial work on resource system. Definitely requires more work to test and verify.

--- basic.py
+++ basic.py
@@ -1,30 +1,31 @@
-#Import and initialize
+# Import and initialize
 import pygame
-pygame.init()
-#Display
-scr=pygame.display.set_mode((1920,1080))
-pygame.display.set_caption("Alt-F4 = QUIT")
-#Entities
 
-##this is where we will do most of the setting up of the assets.
-bg=pygame.Surface(scr.get_size()).convert()
-bg.fill((250,150,50))
-fnt=pygame.font.SysFont("None",24)
-lbl=fnt.render("IDEA-ALTER",1,(255,255,0))
-#Action -> ALTER
+pygame.init()
+# Display
+scr = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption("Alt-F4 = QUIT")
+# Entities
+
+# This is where we will do most of the setting up of the assets.
+bg = pygame.Surface(scr.get_size()).convert()
+bg.fill((250, 150, 50))
+fnt = pygame.font.SysFont("None", 24)
+lbl = fnt.render("IDEA-ALTER", 1, (255, 255, 0))
+# Action -> ALTER
 ################
-#Assign values
-clock=pygame.time.Clock()
+# Assign values
+clock = pygame.time.Clock()
 keepGoing = True
-#Loop
+# Loop
 while keepGoing:
-#Timing
+    # Timing
     clock.tick(30)
-#Events
-    for evnt in pygame.event.get():
-        if evnt.type==pygame.QUIT:
+    # Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             keepGoing = False
-#Refresh display
-    scr.blit(bg,(0,0))
-    scr.blit(lbl,(100,100))
+    # Refresh display
+    scr.blit(bg, (0, 0))
+    scr.blit(lbl, (100, 100))
     pygame.display.flip()
