30373cc4a4567d2edeb4456260eb1da9f53f6966
Kevin Dunn <kevin_dunn@my.uri.edu>
2019-04-03 11:42:16 -0400

    Added new placeholder building and farm textures, added fullscreen mode, and improved and stabalized the framerate. Escape to exit.

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #Kevin Dunn, add names
 #CSC 305
 #Colony Sim Game: Main
-#3/22/2019
+#4/2/2019
 
 import pickle
 import pygame
@@ -14,6 +14,7 @@ from Unit import *
 from Input import *
 from Display import *
 import Config
+import time
    
 
 def main():
@@ -38,7 +39,11 @@ def main():
         pygame.display.update()#update map once updated
 
         #game time
-        pygame.time.wait(Config.frameTime)#pause for a short while 
+        tempTime=time.time()*1000
+        while(tempTime<Config.frameTime+90):
+            tempTime=time.time()*1000
+            pygame.time.wait(1)#pause for a short while
+        Config.frameTime=tempTime
         
     #If loops exits
     pygame.quit()
