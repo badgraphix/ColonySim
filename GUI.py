1082df243b54e3d3f74bd30d8235bfaaaa58e0ac
Aidan Hilt <aidanhilt2@gmail.com>
2019-04-13 01:59:23 -0400

    Final commit for 4/13/19. Fixed a bug involving pressing the back button on the first item of a unit list, added displays for health, thirst, and the current behavior mode of the unit, cleaned up code to remove many redundant, repeated, or unclear calculations. This commit includes the Unit.py from the cbrady branch, to display the updated stats of health and hunger.

--- GUI.py
+++ GUI.py
@@ -104,9 +104,12 @@ class BottomMenu:
     # Spacing constants
     TEXT_OFFSET = 25
 
-    # The font used by this menu
+    # Setting the fonts used by this menu (A larger font for the three items on the side, one for the middle text, and one
+    # the bar labels
     pygame.font.init()
-    font = pygame.font.Font(Config.gameFont, 20)
+    leftFont = None
+    middleFont = None
+    rightFont = None
 
     # The size of boxes for displaying units and buildings. It may be dynamically resized later based on the size of the
     # screen, but for now will be a constant
@@ -115,8 +118,13 @@ class BottomMenu:
     # The index of the currently selected unit in the units list. Defaults to zero
     selectedUnit = 0
 
-    # The progress bar used to display unit hunger, and building health
+    # The progress bar used to display unit health, and building health
     topBar = None
+
+    # The progress bar used to display unit hunger, and building capacity
+    middleBar = None
+
+    # The progress bar used to display unit thirst
 
     # The number of boxes that fit into the middle box, the starting horizontal offset, and the starting
     # vertical offset. Calculated by breakBoxes() during __init__
@@ -129,21 +137,51 @@ class BottomMenu:
     firstVisibleBox = 0
 
     # Text members. Not necessarily constants
-    buildingText = font.render("1. Buildings", True, TEXT_COLOR)
-    unitText = font.render("2. Units", False, TEXT_COLOR)
-    colonyText = font.render("3. Colony View", True, TEXT_COLOR)
+    buildingText = None
+    unitText = None
+    colonyText = None
 
-    centerText = font.render("Colony Overview", True, TEXT_COLOR)
+    centerText = None
+
+    healthText = None
+    hungerText = None
+    thirstText = None
+
+    # Sizing and spacing variables for determining the drawing of items on the menu
+    barHeight = 0
+    menuHeight = 0
 
     def __init__(self, width, height, coverage=.2):
         self.trueHeight = height
         self.height = height * (1 - coverage)
         self.width = width
 
+        self.barHeight = self.height / 24
+        self.menuHeight = self.trueHeight - self.height
+
+        spacing = self.calculateVerticalSpacing(4, self.barHeight)
+
         self.numRows, self.numColumns, self.horizontalOffset, self.verticalOffset = self.breakBoxes()
 
-        self.topBar = Bar(self.TEXT_COLOR, pygame.Color(0, 255, 0), "Top Bar", (2 * self.width / 3) + 20,
-                          self.height + 20, self.width / 3 - 40, self.height / 16)
+        self.topBar = Bar(self.TEXT_COLOR, pygame.Color(0, 255, 0), "Top Bar", (2 * self.width / 3) + self.width / 120,
+                          self.height + spacing, self.width / 6, self.barHeight)
+        self.middleBar = Bar(self.TEXT_COLOR, pygame.Color(0, 255, 0), "Top Bar", (2 * self.width / 3) + self.width / 120,
+                          self.height + 2 * spacing + self.height / 32, self.width / 6, self.barHeight)
+        self.bottomBar = Bar(self.TEXT_COLOR, pygame.Color(0, 255, 255), "Top Bar", (2 * self.width / 3) + self.width / 120,
+                          self.height + 3 * spacing + 2 * self.height / 32, self.width / 6, self.barHeight)
+
+        self.leftFont = pygame.font.Font(Config.gameFont, self.sizeFont(self.menuHeight / 3, self.width / 3, "1. Buildings"))
+        self.buildingText = self.leftFont.render("1. Buildings", True, self.TEXT_COLOR)
+        self.unitText = self.leftFont.render("2. Units", True, self.TEXT_COLOR)
+        self.colonyText = self.leftFont.render("3. Colony View", True, self.TEXT_COLOR)
+
+        self.middleFont = pygame.font.Font(Config.gameFont, self.sizeFont(self.verticalOffset + self.TEXT_OFFSET, self.width / 3, "Colony Overview"))
+        self.centerText = self.middleFont.render("Colony Overview", True, self.TEXT_COLOR)
+
+        self.rightFont = pygame.font.Font(Config.gameFont, self.sizeFont(self.width / 24, self.height / 6, "Hunger"))
+        self.healthText = self.rightFont.render("Health", True, self.TEXT_COLOR)
+        self.hungerText = self.rightFont.render("Hunger", True, self.TEXT_COLOR)
+        self.thirstText = self.rightFont.render("Thirst", True, self.TEXT_COLOR)
 
     def draw(self, surface, units):
         # Drawing the rectangles to form the base of the menu
@@ -155,9 +193,7 @@ class BottomMenu:
 
         # Drawing the text on the side
         surface.blit(self.buildingText, (0, self.height))
-
         surface.blit(self.unitText, (0, self.height + (self.trueHeight * .2) / 3))
-
         surface.blit(self.colonyText, (0, self.height + 2 * (self.trueHeight * .2) / 3))
 
         # Drawing the center text (which can change)
@@ -184,7 +220,40 @@ class BottomMenu:
                                                                         self.height + self.TEXT_OFFSET + verticalOffset - 2,
                                                                         self.boxSize + 2, self.boxSize + 2), 2)
 
-                    self.topBar.draw(surface, units.data[i].hungerPoints / Config.MAX_UNIT_HUNGER)
+                    self.topBar.draw(surface, units.data[i].hitPoints / Config.MAX_UNIT_HUNGER)
+                    self.middleBar.draw(surface, units.data[i].hungerPoints / Config.MAX_UNIT_HUNGER)
+                    self.bottomBar.draw(surface, units.data[i].thirstPoints / Config.MAX_UNIT_HUNGER)
+
+                    textX = self.topBar.x + self.width / 6 + self.width / 72
+                    surface.blit(self.healthText, (textX, self.height + self.menuHeight / 21))
+                    surface.blit(self.hungerText, (textX, self.height + 2 * self.menuHeight / 21 + self.barHeight))
+                    surface.blit(self.thirstText, (textX, self.height + 3 * self.menuHeight / 21 + 2 * self.barHeight))
+
+                    behaviorBoxDimension = self.height / 16
+
+                    pygame.draw.rect(surface, pygame.Color(0, 153, 30), (self.bottomBar.x,
+                                                                          self.bottomBar.y + self.barHeight +
+                                                                          self.calculateVerticalSpacing(4, self.barHeight) / 2,
+                                                                          behaviorBoxDimension, behaviorBoxDimension))
+                    pygame.draw.rect(surface, pygame.Color(0, 0, 0), (self.bottomBar.x + self.barHeight + (self.width / 6 - (4 * self.barHeight)) / 4,
+                                                                         self.bottomBar.y + self.barHeight +
+                                                                         self.calculateVerticalSpacing(4, self.barHeight) / 2,
+                                                                         behaviorBoxDimension, behaviorBoxDimension))
+                    pygame.draw.rect(surface, pygame.Color(255, 0, 0), (self.bottomBar.x + 2 * self.barHeight +
+                                                                         2 * (self.width / 6 - (4 * self.barHeight)) / 4,
+                                                                         self.bottomBar.y + self.barHeight +
+                                                                         self.calculateVerticalSpacing(4, self.barHeight) / 2,
+                                                                         behaviorBoxDimension, behaviorBoxDimension))
+                    pygame.draw.rect(surface, pygame.Color(0, 0, 102), (self.bottomBar.x + 3 * self.barHeight +
+                                                                         3 * (self.width / 6 - (4 * self.barHeight)) / 4,
+                                                                         self.bottomBar.y + self.barHeight +
+                                                                         self.calculateVerticalSpacing(4, self.barHeight) / 2,
+                                                                         behaviorBoxDimension, behaviorBoxDimension))
+                    pygame.draw.rect(surface, pygame.Color(192, 192, 192), (self.bottomBar.x + units.data[i].behavior * self.barHeight +
+                                                                        units.data[i].behavior * (self.width / 6 - (4 * self.barHeight)) / 4,
+                                                                        self.bottomBar.y + self.barHeight +
+                                                                        self.calculateVerticalSpacing(4, self.barHeight) / 2,
+                                                                        behaviorBoxDimension, behaviorBoxDimension), 4)
 
                 horizontalOffset += 50
                 if self.width / 3 + horizontalOffset + self.boxSize > 2 * self.width / 3:
@@ -200,13 +269,13 @@ class BottomMenu:
     def changeMode(self, mode):
         if mode == 1:
             self.mode = 1
-            self.centerText = self.font.render("Building List", True, self.TEXT_COLOR)
+            self.centerText = self.middleFont.render("Building List", True, self.TEXT_COLOR)
         elif mode == 2:
             self.mode = 2
-            self.centerText = self.font.render("Unit List", True, self.TEXT_COLOR)
+            self.centerText = self.middleFont.render("Unit List", True, self.TEXT_COLOR)
         else:
             self.mode = 3
-            self.centerText = self.font.render("Colony Overview", True, self.TEXT_COLOR)
+            self.centerText = self.middleFont.render("Colony Overview", True, self.TEXT_COLOR)
 
     # Input method to change the selected unit by changing the selected unit variable. Is put into its' own function
     # to account for wrapping, and to handle up and down presses (since it is a 1-D list).
@@ -223,12 +292,14 @@ class BottomMenu:
             self.selectedUnit = (self.selectedUnit - 1)
             if self.selectedUnit < 0:
                 self.selectedUnit = len(units.data) - 1
-                self.firstVisibleBox = self.numColumns * (len(units.data) // self.numColumns)
+                if len(units.data) > self.numColumns * self.numRows:
+                    self.firstVisibleBox = self.numColumns * (len(units.data) // self.numColumns)
             elif self.selectedUnit < self.firstVisibleBox:
                 self.firstVisibleBox -= self.numColumns
 
         elif change == -10:
             self.selectedUnit -= self.numColumns
+
             if self.selectedUnit < self.firstVisibleBox:
                 if self.selectedUnit < 0:
                     if self.numColumns + self.selectedUnit + self.numColumns * \
@@ -251,12 +322,12 @@ class BottomMenu:
 
                     self.firstVisibleBox = 0
                     self.selectedUnit = (self.selectedUnit - self.numColumns) % self.numColumns
-                elif self.selectedUnit < len(units.data):
+                elif self.selectedUnit <= len(units.data):
                     self.firstVisibleBox += self.numColumns
                 else:
                     self.selectedUnit -= self.numColumns
-
-        print(self.selectedUnit, self.firstVisibleBox)
+            elif self.selectedUnit >= len(units.data):
+                self.selectedUnit -= self.numColumns
 
     # Method that breaks up how many boxes will fit into the middle square, and sets the padding accordingly. Will not
     # be perfect
@@ -273,10 +344,27 @@ class BottomMenu:
         totalSpace = 25
         rows = 0
 
-        while totalSpace + self.boxSize < self.trueHeight - self.height:
+        while totalSpace + self.boxSize < self.menuHeight:
             rows += 1
             totalSpace += self.boxSize + 10
 
-        verticalOffset = (self.trueHeight - self.height - totalSpace) / 2
+        verticalOffset = (self.menuHeight - totalSpace) / 2
 
         return rows, columns, horizontalOffset, verticalOffset
+
+    # Method used to determine vertical spacing between objects of equal size
+    def calculateVerticalSpacing(self, numObjects, objectHeight):
+        return (self.menuHeight - objectHeight * numObjects) / numObjects
+
+    # Method that sizes a font to maximally fill out a given space
+    def sizeFont(self, verticalSpace, horizontalSpace, renderText):
+        size = 10
+        testFont = pygame.font.Font(Config.gameFont, size)
+        testText = testFont.render(renderText, True, self.TEXT_COLOR)
+
+        while testFont.get_linesize() < verticalSpace and testText.get_width() < horizontalSpace - horizontalSpace * .05:
+            size += 1
+            testFont = pygame.font.Font(Config.gameFont, size)
+            testText = testFont.render(renderText, True, self.TEXT_COLOR)
+
+        return size
--- Unit.py
+++ Unit.py
@@ -1,59 +1,84 @@
-#Cam Brady, John Bertsch, Kevin Dunn
-#CSC 305
-#Colony Sim Game: Units
-#3/25/2019
+# Cam Brady, John Bertsch, Kevin Dunn
+# CSC 305
+# Colony Sim Game: Units
+# 2/28/2019
 
-import pickle
-import pygame
-from pygame.locals import *
-import sys, random, os.path
-import math
-from Tile import *
+import random
 import Config
 
 
-
 class Actor:
-    data=[]
-    totalActors=0
-    def __init__(self, num,x,y):
-        self.totalActors=num
-        for temp in range(0,num):
-            unitTemp=unit(random.randint(0,x),random.randint(0,y), temp)
+    data = []
+    totalActors = 0
+
+    def __init__(self, num, x, y):
+        self.totalActors = num
+        for temp in range(0, num):
+            unitTemp = unit(random.randint(0, x), random.randint(0, y), temp)
             self.data.append(unitTemp)
+
     def allAct(self):
-        for temp in range(0,self.totalActors):
+        for temp in range(0, self.totalActors):
             self.data[temp].perform()
+
+
+# focusTileTypes: when pathfinding, the unit will move towards the closest tile that is one of these type
+# Strategies for priority queue
+class FarmerStrategy:
+    focusTileTypes = [4]  # Farmland
+
+
+class WoodcutterStrategy:
+    focusTileTypes = [1]  # Forest
+
+
+class SoldierStrategy:
+    focusTileTypes = None
+
+
+class IdleStrategy:
+    focusTileTypes = None
+
 
 class unit:
     # Coordinates
     xPos = 0
     yPos = 0
-    #Target coordinates
+    # Target coordinates
     targetXPos = None
     targetYPos = None
-    # Hunger. When it hits 0, the unit dies.
-
-    #hp
+    hitPoints = 100
+    # Hunger. When it hits 0, the unit will lose 1 HP every tick.
     hungerPoints = 100
+    thirstPoints = 100
     # Unit type. Represented as an integer that is used as the parameter for a getUnitData() function.
     unitType = 1
-    # Inventory. Each entry in the array represents the quantity of that respective resource type.
-    # A unit can only hold ONE type of resource at a time, so keep this in mind while developing.
+    # Inventory. Each entry in the array represents the quantity of that respective resource type. A unit can only hold ONE type of resource at a time, so keep this in mind while developing.
     inventory = [0, 0, 0, 0, 0, 0]
-    # Each behavior type is currently stored as an int. 0 is standby mode, where the unit will not perform any actions.
-    behavior = 0
+    behavior = 0  # Each behavior type is currently stored as an int. 0 is standby mode, where the unit will not perform any actions.
     targetTile = None  # Specifies where the unit is heading towards. Does not always contain a value.
-    priorityQueue = [1, 2, 3, 4, 5]
+    priorityQueue = [1, 2, 4, 3]
 
     def __init__(self, x, y, unitID):
         self.unitID = unitID
         self.setXPos(x)
         self.setYPos(y)
-        self.setInPriorityQueue(1,0)
+        # self.setInPriorityQueue(1,0)
 
     def setInventory(self, resourceType, quantity):
         self.inventory[resourceType] += quantity
+
+    def getCurrentStrategy(self, index):
+        if index == None:
+            index = 0
+        if self.priorityQueue[index] == 1:
+            return FarmerStrategy
+        elif self.priorityQueue[index] == 2:
+            return WoodcutterStrategy
+        elif self.priorityQueue[index] == 3:
+            return SoldierStrategy
+        elif self.priorityQueue[index] == 4:
+            return IdleStrategy
 
     def setBehavior(self, behaviorType):
         self.behavior = behaviorType
@@ -71,6 +96,15 @@ class unit:
 
     def getBehavior(self):
         return self.behavior
+
+    def getHungerPoints(self):
+        return self.hungerPoints
+
+    def getThirstPoints(self):
+        return self.thirstPoints
+
+    def getHitPoints(self):
+        return self.hitPoints
 
     def getXPos(self):
         return self.xPos
@@ -100,6 +134,13 @@ class unit:
     def setTargetYPos(self, val):
         self.targetYPos = val % Config.gameMap.ySize
 
+    def setTargetPos(self, x, y):
+        self.setTargetXPos(x);
+        self.setTargetYPos(y);
+
+    def isUnitAtTargetPos(self):
+        return self.getXPos() == self.getTargetXPos() and self.getYPos() == self.getTargetYPos();
+
     def translatePosition(self, x, y):
         currentTile = Config.gameMap.getTile(self.getXPos(), self.getYPos())
         currentTile.setStationedUnitID(-1)
@@ -108,53 +149,90 @@ class unit:
         newTile = Config.gameMap.getTile(self.getXPos(), self.getYPos())
         newTile.setStationedUnitID(self.unitID)
 
+    def reduceHungerPoints(self):
+        if self.hungerPoints - 1 < 0:  # starve
+            self.hungerPoints = 0
+        else:
+            self.hungerPoints -= 1;
+        if self.hungerPoints >= 80:  # Heal if the user is well-fed.
+            self.hitPoints += 1
+            if self.hitPoints > 100:  # Enforce the cap
+                self.hitPoints = 100;
+
     def collectResource(self):
-        # Farms resource on the tile the unit is standing on. This is fired for all units on every tick,
-        # if it is possible for them to farm something.
-        currentTile = self.getCurrentTile() # TODO: add a function in Tile that lets us remove a resource from it and return it here.
-        resourceData = currentTile.collectResource(1) #TODO: add a function to Tile called collectResource() that returns .type (resource type) and .amount.
-        self.inventory[resourceData[0]] = self.inventory[resourceData[0]] + resourceData[1] #Add that to unit's inventory.
-        #print("Farming resource. Quantity ", resourceData[1], " of type ", resourceData[0])
+        # Farms resource on the tile the unit is standing on. This is fired for all units on every tick, if it is possible for them to farm something.
+        currentTile = self.getCurrentTile()
+        resourceData = currentTile.collectResource(1)
+        self.inventory[resourceData[0]] = self.inventory[resourceData[0]] + resourceData[
+            1]  # Add that to unit's inventory.
+        print("Farming resource. Quantity ", resourceData[1], " of type ", resourceData[0])
 
-    def findClosestTileOfType(self, destinationTileType):
-        #TODO: Basically this whole function.
-        targetTile = None
-        tiles = Config.gameMap.data
-        #print("TOTAL TILES " + str(len(tiles)))
+    def findPosOfClosestTileOfType(self,
+                                   destinationTileTypeArray):  # Returns closest potential target tile instance given the array of tile types it could be.
+        # Is the unit already on a potential target tile?
+        if self.isTileOfType(Config.gameMap.getTile(self.getXPos(), self.getYPos()), destinationTileTypeArray) == True:
+            return [self.getXPos(), self.getYPos()]
+
         # Search for the nearest tile of tileType
-        mapSizeX = Config.gameMap.xSize
-        mapSizeY = Config.gameMap.ySize
         tileFound = False
-        for x in range(0,mapSizeX):  # This should check all tiles around the unit, then the ones around them, and so on, continuing farther out each time.
-            for y in range(0,mapSizeY):
-                if not tileFound:
-                    tile = Config.gameMap.getTile(x,y)  # Right now we are using a much dumber algorithm to test with.
-                    tileType = tile.getType()
-                    #print(tileType, " vs ", destinationTileType)
-                    if tileType == destinationTileType:
-                        #print("Setting to ", x, ",", y)
-                        self.setTargetXPos(x)
-                        self.setTargetYPos(y)
-                        tileFound = True
-                else:
+        maxDistance = 5  # The farthest distance the unit will bother scanning for tiles
+        distance = 1  # The shortest distance the unit will bother scanning. This should probably start at 1.
+        while tileFound == False:  # check all spaces [distance] tiles away from the unit
+            x = distance
+            y = 0
+            tileFound = False
+            while tileFound == False:  # Check each position that adds up to the specified distance.
+                unitXPos = self.getXPos()
+                unitYPos = self.getYPos()
+                targetXPosArray = [unitXPos - x, unitXPos + x, unitXPos - x, unitXPos + x]
+                targetYPosArray = [unitYPos - y, unitYPos - y, unitYPos + y, unitYPos + y]
+                for i in range(0, 3):
+                    targetXPos = targetXPosArray[i]
+                    targetYPos = targetYPosArray[i]
+                    if self.isTileOfType(Config.gameMap.getTile(targetXPos, targetYPos),
+                                         destinationTileTypeArray) == True:
+                        return [targetXPos, targetYPos]
+                    # No tile found [distance] tiles away that's a target tile.
+                x -= 1
+                y += 1
+                if y > distance:  # We have checked every tile [distance] tiles away.
                     break
-            if tileFound:
+            distance += 1  # Increase distance and check all tiles that distance away.
+            if distance > maxDistance:  # Don't bother checking tiles more than the specified max distance away..
+                # print("No tile found")
                 break
-    def isUnitAtTargetPos(self): #Returns true if unit is at target position, returns false if it is not.
-        return (self.getXPos() == self.getTargetXPos()) and (self.getYPos() == self.getTargetYPos())
 
-    def perform(self):  # This is fired every tick. What action the unit performs is dependent on its behavior, as well as external factors.
-        if self.getPriorityQueue()[0] == 1:  # Continuously travel right
-            self.translatePosition(1, 0)
-        elif self.getPriorityQueue()[0] == 2:  # Travel to the nearest tile of type 2.
-            if self.getTargetXPos() == None:
-                self.findClosestTileOfType(2) #sets as target
+    def isTileOfType(self, tile, destinationTileTypeArray):
+        # Note: This function was written to simplify the structure of findClosestTileOfType. It was not really intended for general use.
+        for destinationTileType in destinationTileTypeArray:
+            tileType = tile.getType()
+            if tileType == destinationTileType:
+                return True
 
-            if self.isUnitAtTargetPos(): #If you are on the target tile, start collecting resources!
-                self.collectResource()
+    def perform(
+            self):  # This is fired every tick. What action the unit performs is dependent on its behavior, as well as external factors.
+        self.reduceHungerPoints()
+        # print('Top priority: ', self.getPriorityQueue()[0])
+        priorityIndex = 0
+        while self.getTargetXPos() == None:
+            currentStrategy = self.getCurrentStrategy(priorityIndex)
+            if currentStrategy.focusTileTypes:
+                # Find target tile type of closet position
+                targetPos = self.findPosOfClosestTileOfType(
+                    currentStrategy.focusTileTypes)  # returns closest potential target tile
+                if targetPos:
+                    self.setTargetPos(targetPos[0], targetPos[1])
+                else:  # Tile not found! Try next strategy.
+                    priorityIndex += 1
+            else:
+                break  # This priority has no target tiles (meaning it asks us to idle). That's managable, so let's break the loop and settle on this!
+        if self.isUnitAtTargetPos() == True:  # If you are on the target tile, start collecting resources!
+            self.collectResource()
 
-            #TODO: If one unit is heading toward a tile, should other units try heading somewhere else?
-            #Pathfind to (self.targetXPos, self.targetYPos)
+        # TODO: If one unit is heading toward a tile, should other units try heading somewhere else?
+        # Pathfind to (self.targetXPos, self.targetYPos)
+        if self.getTargetXPos():
+            # print('At ', self.getTargetXPos(), ' going to ', self.getXPos())
             if self.getTargetXPos() < self.getXPos():
                 self.translatePosition(-1, 0)
             elif self.getTargetXPos() > self.getXPos():
@@ -164,4 +242,3 @@ class unit:
                 self.translatePosition(0, -1)
             elif self.getTargetYPos() > self.getYPos():
                 self.translatePosition(0, 1)
-
